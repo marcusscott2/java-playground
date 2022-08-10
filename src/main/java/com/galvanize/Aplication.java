c87b8b32428cced20f08feb029662e03204867ff
Marcus Scott <marcus.a.scott12.mil@swf.army.mil>
2022-08-10 13:15:48 -0500

    intial commit

---
+++ src/main/java/com/galvanize/Aplication.java
@@ -0,0 +1,10 @@
+package com.galvanize;
+
+import java.util.Arrays;
+
+public class Aplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
